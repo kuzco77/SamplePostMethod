35740492bd3b8cfc1268e4a878f4c1ecbd4cf315
Dung LT <dunglt@bkindex.com>
2016-07-05 16:17:14 +0700

    Initial Commit

---
+++ SamplePostMethod/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SamplePostMethod
+//
+//  Created by Owner on 7/5/16.
+//  Copyright © 2016 Owner. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SamplePostMethod/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  SamplePostMethod
+//
+//  Created by Owner on 7/5/16.
+//  Copyright © 2016 Owner. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
